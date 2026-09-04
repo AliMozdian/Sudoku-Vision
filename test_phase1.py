ee8427eb852697d52c8fda40f3f08646ff0b0f5c
Ali <alimozdian@gmail.com>
2026-09-04 22:55:26 +0330

    test-cells added to test_phase1
    for most of the images it works find, but for some others noooo!
    
    cutting through digits, including lines in boxes, tilted weird cells, and many more problems

--- test_phase1.py
+++ test_phase1.py
@@ -1,12 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
-from src.board_extractor import preprocess_image, find_board_corners, warp_perspective
+from src.board_extractor import preprocess_image, find_board_corners, warp_perspective, extract_cells
+
+
 
 def cut_and_save(filename: str, src_dir: str, dest_dir: str, display=False):
     img_path = src_dir + filename
     image = cv2.imread(img_path)
-    w, h, clr= image.shape
-    image = cv2.resize(image, (h//2, w//2))
+    # w, h, clr= image.shape
+    # image = cv2.resize(image, (h//2, w//2))
     if display:
         cv2.imshow('original-'+filename, image)
         cv2.waitKey()
@@ -43,7 +45,8 @@ def cut_and_save(filename: str, src_dir: str, dest_dir: str, display=False):
             cv2.waitKey()
 
 
-def main():
+
+def test_cut_all():
     """cut all and save them"""
     src_dir = "data/raw/v2_train/"
     dst_dir = "data/processed/tests/"
@@ -56,5 +59,52 @@ def main():
        cv2.destroyAllWindows()
     # cut_and_save('image1066.jpg', src_dir, dst_dir, display=True)
 
+
+
+def make_grid_cells(filename: str, src_dir: str, dest_dir: str, display=False):
+    """Load a clean sample"""
+    img_path = src_dir + filename
+    image = cv2.imread(img_path)
+    if image is None:
+        raise FileNotFoundError(f"Could not load the image")
+
+
+    thresh = preprocess_image(image)
+    corners = find_board_corners(thresh)
+
+    if corners is not None:
+        warped = warp_perspective(image, corners, output_size=450)
+        cells = extract_cells(warped, margin_ratio=0.12)
+
+        # Plot all 81 cells in a 9x9 subplot grid
+        fig, axes = plt.subplots(9, 9, figsize=(9, 9))
+        for idx, ax in enumerate(axes.flat):
+            cell_rgb = cv2.cvtColor(cells[idx], cv2.COLOR_BGR2RGB)
+            ax.imshow(cell_rgb)
+            ax.axis('off')
+
+        # Replace plt.show() with:
+        plt.tight_layout()
+        plt.savefig(f"data/processed/tests/cropped-cells/grid-cells-{filename}.png")
+        print("Saved grid preview to data/processed/tests/cropped-cells/grid-cells-{filename}.png")
+
+    else:
+        print("Could not extract board.")
+
+
+def test_cells():
+    """cut all and extract cells and then save them in grids by matplotlib"""
+    src_dir = "data/raw/v2_train/"
+    dst_dir = "data/processed/tests/"
+
+    with open('./data/raw/v2_train.desc', 'r') as f:
+        fnames = list(map(lambda name: name.split('/')[1][:-1], f.readlines()))
+
+    for fn in fnames:
+       make_grid_cells(fn, src_dir, dst_dir)
+    
+
+
 if __name__ == '__main__':
-    main()
+    # test_cut_all()
+    test_cells()
